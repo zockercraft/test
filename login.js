046e302942ecda00dfe788e07dc2ef0724986577
zockercraft <117124606+zockercraft@users.noreply.github.com>
2022-11-01 17:02:54 +0100

    Update login.js

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ function login()
     let usermane = document.getElementById("username").value;
     let password = document.getElementById("password").value;
     if(usermane == "admin" && password == "abc") {
-        window.location.href = './index.html';
+        window.location.href = './index';
     }
     else
     {
